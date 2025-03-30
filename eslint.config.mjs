415150a581bd0ad0106dc389b05ee4926c1db8f6
OtsukaTomoaki <ootsuka.ootsuka.ootsuka.26@gmail.com>
2025-03-30 12:29:26 +0900

    Fix ESLint configuration for CI

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,8 +2,13 @@
 import withNuxt from './.nuxt/eslint.config.mjs'
 
 export default withNuxt({
-  // Your custom configs here
-  env: {
-    jest: true,
+  languageOptions: {
+    globals: {
+      // 必要なグローバル変数を定義します
+      isVue2: 'readonly',
+      isVue3: 'readonly',
+      // 他のグローバル変数もここに追加
+    },
   },
-});
+  // 他のカスタム設定があればここに追加
+})

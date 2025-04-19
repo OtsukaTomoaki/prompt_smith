a314fb7aec08a30ee998489b362bd1410fc9b98b
OtsukaTomoaki <ootsuka.ootsuka.ootsuka.26@gmail.com>
2025-04-19 11:39:58 +0900

    ダークモードに修正

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,26 +1,9 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
-  devtools: { enabled: true },
-
-  modules: [
-    '@nuxt/content',
-    '@nuxt/eslint',
-    '@nuxt/fonts',
-    '@nuxt/icon',
-    '@nuxt/image',
-    '@nuxt/scripts',
-    '@nuxt/test-utils',
-    '@nuxt/ui',
-    '@nuxtjs/tailwindcss'
-  ],
+  css: ['@/assets/css/tailwind.css'],
   postcss: {
     plugins: {
       tailwindcss: {},
       autoprefixer: {},
     },
   },
-  css: [
-    '@/assets/css/tailwind.css',
-  ]
 })

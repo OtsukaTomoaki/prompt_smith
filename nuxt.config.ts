0a89647a4f490c27db723a3a3aad5822e40638f2
OtsukaTomoaki <ootsuka.ootsuka.ootsuka.26@gmail.com>
2025-04-27 18:04:10 +0900

    ログイン機能を実装

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -7,6 +7,12 @@ export default defineNuxtConfig({
       autoprefixer: {},
     },
   },
+  runtimeConfig: {
+    public: {
+      supabaseUrl: process.env.SUPABASE_URL,
+      supabaseAnonKey: process.env.SUPABASE_ANON_KEY,
+    },
+  },
 
   compatibilityDate: '2025-04-19',
 })
---
+++ plugins/supabase.ts
@@ -0,0 +1,18 @@
+// plugins/supabase.ts
+import { createClient, type SupabaseClient  } from '@supabase/supabase-js'
+
+export default defineNuxtPlugin(() => {
+
+  const config = useRuntimeConfig()
+
+  const supabase = createClient(
+    config.public.supabaseUrl as string,
+    config.public.supabaseAnonKey as string
+  )
+
+  return {
+    provide: {
+      supabase: supabase as SupabaseClient,
+    },
+  }
+})

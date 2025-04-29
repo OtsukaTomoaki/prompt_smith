bb51a4844301c078675d35a46268f09f6b5b4d86
OtsukaTomoaki <ootsuka.ootsuka.ootsuka.26@gmail.com>
2025-04-29 13:04:00 +0900

    セッションを持っていてもログイン画面に飛ばされるバグを修正

--- middleware/auth.global.ts
+++ middleware/auth.global.ts
@@ -2,14 +2,17 @@ import { defineNuxtRouteMiddleware, navigateTo } from 'nuxt/app'
 import type { SupabaseClient } from '@supabase/supabase-js'
 
 export default defineNuxtRouteMiddleware(async (to) => {
-  // ① /login だけは認証チェックをスキップする
+  // ① サーバーサイドでは認証チェックをスキップする
+  if (process.server) {
+    return
+  }
+  // ② /login だけは認③チェックをスキップする
   if (to.path === '/login') {
-    console.log('Skip auth check for /login')
     return
   }
   const { $supabase } = useNuxtApp()
   const { data } = await ($supabase as SupabaseClient).auth.getUser()
-  // ② 未ログインなら /login にリダイレクト
+  // ③ 未ログインなら /login にリダイレクト
   if (!data.user) {
     return navigateTo('/login')
   }
